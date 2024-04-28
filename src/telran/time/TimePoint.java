fca1f9f912429b293b74492d2cd6da17e26680aa
user.emial <emil.mehraliyev003@gmail.com>
2024-04-28 16:29:20 +0300

    Update

--- src/telran/time/TimePoint.java
+++ src/telran/time/TimePoint.java
@@ -1,46 +1,51 @@
 package telran.time;
 
-public class TimePoint implements Comparable<TimePoint>{
+public class TimePoint implements Comparable<TimePoint> {
 	int amount;
 	TimeUnit timeUnit;
+
 	public TimePoint(int amount, TimeUnit timeUnit) {
 		this.amount = amount;
 		this.timeUnit = timeUnit;
 	}
+
 	public int getAmount() {
 		return amount;
 	}
+
 	public TimeUnit getTimeUnit() {
 		return timeUnit;
 	}
+
 	public TimePoint convert(TimeUnit unit) {
 		int amountInSeconds = this.amount * this.timeUnit.getValue();
 		int newAmount = amountInSeconds / unit.getValue();
 		return new TimePoint(newAmount, unit);
 	}
+
 	public TimePoint with(TimePointAdjuster adjuster) {
-		if(adjuster == null) {
+		if (adjuster == null) {
 			return this;
-		} 
+		}
 		return adjuster.adjust(this);
 	}
+
 	@Override
 	public int compareTo(TimePoint o) {
 		int thisSeconds = this.amount * this.timeUnit.getValue();
 		int otherSeconds = o.getAmount() * o.getTimeUnit().getValue();
-		
+
 		return thisSeconds - otherSeconds;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if(!(obj instanceof TimePoint)) {
+		if (!(obj instanceof TimePoint)) {
 			return false;
 		}
 		TimePoint other = (TimePoint) obj;
-	    int thisInSeconds = this.amount * this.timeUnit.getValue();
-	    int otherInSeconds = other.getAmount() * other.getTimeUnit().getValue();
+		int thisInSeconds = this.amount * this.timeUnit.getValue();
+		int otherInSeconds = other.getAmount() * other.getTimeUnit().getValue();
 		return thisInSeconds == otherInSeconds;
 	}
-	
-	
 }
